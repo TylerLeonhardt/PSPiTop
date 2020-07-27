cf5400416699f6d341e642024549481c117d54ca
Tyler Leonhardt <tylerl0706@gmail.com>
2020-07-27 14:18:13 -0700

    directory

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@ dotnet publish $PSScriptRoot/src
 
 $outputDir = "$PSScriptRoot/out/PSPiTop" 
 Remove-Item $outputDir -Force -Recurse -ErrorAction SilentlyContinue
-New-Item $outputDir -Force
+New-Item $outputDir -Force -ItemType Directory
 
 # Copy bin
 Get-ChildItem "$PSScriptRoot/src/bin/$Configuration/netcoreapp3.1/publish" -File | Copy-Item -Destination $outputDir

b31b98121f439bd73bd6fea71bcc8c88907461c5
Tyler Leonhardt <tylerl0706@gmail.com>
2020-07-27 14:13:20 -0700

    build fixes

--- build.ps1
+++ build.ps1
@@ -1,13 +1,19 @@
 #requires -Version 7.0
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Configuration = "Debug"
+)
 
 dotnet publish $PSScriptRoot/src
 
 $outputDir = "$PSScriptRoot/out/PSPiTop" 
 Remove-Item $outputDir -Force -Recurse -ErrorAction SilentlyContinue
-New-Item $outputDir
+New-Item $outputDir -Force
 
-Get-ChildItem "$PSScriptRoot/src/bin/$Configuration/netcoreapp3.1/publish" -File
-| Copy-Item $outputDir
+# Copy bin
+Get-ChildItem "$PSScriptRoot/src/bin/$Configuration/netcoreapp3.1/publish" -File | Copy-Item $outputDir
 
-Get-ChildItem "$PSScriptRoot/src/PSPiTop.psd1"
-| Copy-Item $outputDir
+# Copy in psd1
+Get-ChildItem "$PSScriptRoot/src/PSPiTop.psd1" | Copy-Item $outputDir
